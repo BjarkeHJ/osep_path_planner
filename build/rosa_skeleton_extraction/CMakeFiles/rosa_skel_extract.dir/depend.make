583cb72beb34fe685d2460941a4a8ce7e4b9798e
BjarkeHJ <104014101+BjarkeHJ@users.noreply.github.com>
2025-08-13 12:24:27 +0200

    Delete build directory

--- build/rosa_skeleton_extraction/CMakeFiles/rosa_skel_extract.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rosa_skel_extract.
-# This may be replaced when dependencies are built.
--- build/rosa_skeleton_extraction/CMakeFiles/rosa_skeleton_extraction_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rosa_skeleton_extraction_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rosa_skeleton_extraction_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
